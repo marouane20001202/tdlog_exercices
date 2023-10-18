c3b95c5d244f2b524f71bc2cb1e9b8ce2a71f10a
Marouane Rbib <hafidahafidaait15@gmail.com>
2023-10-18 22:25:17 +0100

    Mon message de commit

--- exo2/exo2.py
+++ exo2/exo2.py
@@ -8,8 +8,8 @@ Examples:
     solution('abc', 'd') # returns false
 """
 
-"""
-Create unit test using those cases:
+
+#Create unit test using those cases:
 fixed_tests_True = (
     ( "samurai", "ai"    ),
     ( "ninja",   "ja"    ),
@@ -27,12 +27,35 @@ fixed_tests_False = (
     ( "this",    "fails" ),
     ( "spam",    "eggs"  )
 )
+
 """
 import unittest
+class  Def:
+    def __init__(self, fixed_tests_False, fixed_tests_True):
+        self.fixed_tests_False = fixed_tests_False
+        self.fixed_tests_True = fixed_tests_True
+
+
+
 
 class Exo2Test(unittest.TestCase):
-
+    w = Def(fixed_tests_False, fixed_tests_True)
     def test_text_construction(self):
-        for i in range (len(word[0])):
-             if not self.assertEqual(word[0][i:], word[1]):
-                 continue
+        for j in range(len(self.w[0])):
+            for i in range(len(self.fixed_tests_True[0])):
+                 if not self.assertEqual(self.fixed_tests_False[0][i:], self.fixed_tests_False[1]):
+                     continue
+        print("false test")
+
+"""
+def solution(string, ending):
+    return string.endswith(ending)
+
+# Unit tests for true cases
+for test_case in fixed_tests_True:
+    input_string, ending_string = test_case
+    assert solution(input_string, ending_string) == True
+# Unit tests for false cases
+for test_case in fixed_tests_False:
+    input_string, ending_string = test_case
+    assert solution(input_string, ending_string) == False
